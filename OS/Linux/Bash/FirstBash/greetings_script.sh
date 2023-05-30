fb456b98cecefe523aeb857ed7367df6f73f0fc6
mohammad junaed <mohammadjunaed858@gmail.com>
2023-05-30 14:55:44 +0500

    added file about auditing, mountUnmount and Bashing

---
+++ OS/Linux/Bash/FirstBash/greetings_script.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# Prompt the user for their name
+echo "What is your name?"
+read name
+
+# Greet the user
+echo "Hello, $name!"
+
+# Display the current date
+current_date=$(date +%Y-%m-%d)
+echo "Today's date is: $current_date"
+
